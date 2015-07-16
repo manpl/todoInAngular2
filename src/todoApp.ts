d2bfcc092f103bb76bd6c0c0619d125cc5e5feb7
Marcinn <nawrockim@op.pl>
2015-07-16 22:40:01 +0200

    displaying items

--- src/todoApp.ts
+++ src/todoApp.ts
@@ -1,20 +1,52 @@
 /// <reference path="typings/angular2/angular2.d.ts" />
-import {Component, View, bootstrap} from 'angular2/angular2';
+import {Component, View, bootstrap, NgFor} from 'angular2/angular2';
 
 // Annotation section
 @Component({
-  selector: 'my-app'
+	selector: 'todo-list',
+	//appInjector: [ItemsLocalStorageRepository] 
 })
 @View({
-  template: '<h1>Hello {{ name }}</h1>'
+  templateUrl: 'templates/todo.html',
+  directives: [NgFor]
 })
 // Component controller
 class todoApp {
-  name: string;
-  
-  constructor() {
-    this.name = 'todo list';
-  }
+	name: string;
+	storage: ItemsRepository;
+	items: Array<String>;
+	
+	constructor(){//itemsRepository: ItemsLocalStorageRepository) {
+		this.name = 'todo list';
+		this.storage = new ItemsLocalStorageRepository();
+		this.items = this.storage.getAll();
+	}
+}
+
+interface ItemsRepository{
+	add();
+	remove();
+	getAll();
+}
+
+class ItemsLocalStorageRepository implements ItemsRepository{	
+	items: Array<String>;
+
+	constructor(){
+		this.items = ['angular2', 'angular', 'something else'];
+	}
+	
+	add(){
+		
+	}
+	
+	remove(){
+		
+	}
+	
+	getAll() : Array<String>{
+		return this.items;
+	}
 }
 
 bootstrap(todoApp); 
