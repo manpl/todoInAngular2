1245f475aba50d2ddba7793c987ce5e30d3191b4
Marcinn <nawrockim@op.pl>
2015-07-26 23:23:17 +0200

    passing repository to constructor

--- src/todoApp.ts
+++ src/todoApp.ts
@@ -1,35 +1,14 @@
 /// <reference path="typings/angular2/angular2.d.ts" />
 import {Component, View, bootstrap, NgFor} from 'angular2/angular2';
 
-// Annotation section
-@Component({
-	selector: 'todo-list',
-	//appInjector: [ItemsLocalStorageRepository] 
-})
-@View({
-  templateUrl: 'templates/todo.html',
-  directives: [NgFor]
-})
-// Component controller
-class todoApp {
-	name: string;
-	storage: ItemsRepository;
-	items: Array<String>;
-	
-	constructor(){//itemsRepository: ItemsLocalStorageRepository) {
-		this.name = 'todo list';
-		this.storage = new ItemsLocalStorageRepository();
-		this.items = this.storage.getAll();
-	}
-}
-
-interface ItemsRepository{
+interface ItemsRepository {
 	add();
 	remove();
 	getAll();
 }
 
-class ItemsLocalStorageRepository implements ItemsRepository{	
+
+class ItemsLocalStorageRepository implements ItemsRepository {	
 	items: Array<String>;
 
 	constructor(){
@@ -49,4 +28,29 @@ class ItemsLocalStorageRepository implements ItemsRepository{
 	}
 }
 
-bootstrap(todoApp); 
+// Annotation section
+@Component({
+	selector: 'todo-list',
+	injectables: [ItemsLocalStorageRepository]	
+})
+@View({
+  templateUrl: 'templates/todo.html',
+  directives: [NgFor]
+})
+// Component controller
+class todoApp {
+	name: string;
+	storage: ItemsRepository;
+	items: Array<String>;
+	 
+	constructor(itemsRepository: ItemsLocalStorageRepository ) {
+		
+		console.log(arguments);
+		
+		this.name = 'todo list';
+		this.storage = new ItemsLocalStorageRepository();
+		this.items = this.storage.getAll();
+	}
+}
+
+bootstrap(todoApp, [ItemsLocalStorageRepository]); 
